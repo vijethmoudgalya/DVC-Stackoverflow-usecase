6bc5ae413385b822600d2840525d2f99945a42ae
Vijeth Moudgalya <vijethmoudgalya@gmail.com>
2021-10-30 08:33:12 +0530

    setup update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC-Stackoverflow-usecase"
+AUTHOR_USER_NAME = "vijeth_moudgalya"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
